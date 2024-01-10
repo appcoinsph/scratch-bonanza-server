3e15aeb2e5ceb61730235e800a357581712eeae7
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-10 12:08:18 +0800

    Update cardComposer.ts

--- src/services/cardComposer.ts
+++ src/services/cardComposer.ts
@@ -38,11 +38,11 @@ export function getValues(): (0 | 0.00004 | 0.0001 | 0.00200 | 1)[] {
 
     // Value  Probability
 
-    // 0     55%   0.04
-    // 0.00004     35%   0.03
+    // 0     7%   0.04
+    // 0.00004     1%   0.03
     // 0.0001     2%   0.15
-    // 0.00200     1%   0.5
-    // 1    7%   0.01
+    // 0.00200     35%   0.5
+    // 1    55%   0.01
 
 
     // Assign a reward value based on the generated random number
