6f86f415366d32398e934acb3b7023d0e84f3978
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-23 11:19:31 +0800

    Update cardComposer.ts

--- src/services/cardComposer.ts
+++ src/services/cardComposer.ts
@@ -19,9 +19,9 @@ try {
  * Generates an array of values for the scratchcard.
  * Each value represents the reward amount for a specific scratch area.
  */
-export function getValues(): (0.00200 | 0.00004 | 0.0001 | 0.00200 | 0.00200)[] {
+export function getValues(): (0.00200 | 0.00004 | 0.25 | 0.00200 | 0.00200)[] {
   // Array to store the generated values
-  const values: (0.00200 | 0.00004 | 0.0001 | 0.00200 | 0.00200)[] = [];
+  const values: (0.00200 | 0.00004 | 0.25 | 0.00200 | 0.00200)[] = [];
 
   // Generate values for each position on the scratchcard
   for (let i = 0; i < 4; i++) {
@@ -39,7 +39,7 @@ export function getValues(): (0.00200 | 0.00004 | 0.0001 | 0.00200 | 0.00200)[] 
 // Value  Probability
     // 0.00200     51%   0.51
     // 0.00004      30%   0.3
-    // 0.0001     15%   0.15
+    // 0.25     15%   0.15
     // 0.00200     4%   0.04
     // 0.00200    0%   0.00
 
