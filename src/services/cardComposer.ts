36c4789a31dad19d73cf8c4fb3820b039f10a498
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-28 09:38:11 +0800

    Update cardComposer.ts

--- src/services/cardComposer.ts
+++ src/services/cardComposer.ts
@@ -19,9 +19,9 @@ try {
  * Generates an array of values for the scratchcard.
  * Each value represents the reward amount for a specific scratch area.
  */
-export function getValues(): (0.00200 | 0.00200 | 0.25| 0.00004 | 0.00004)[] {
+export function getValues(): (0.00200 | 0.00200 | 0.00004 | 0.00004 | 0.25)[] {
   // Array to store the generated values
-  const values: (0.00200 | 0.00200 | 0.25 | 0.00004 | 0.00004)[] = [];
+  const values: (0.00200 | 0.00200 | 0.00004 | 0.00004 | 0.25)[] = [];
 
   // Generate values for each position on the scratchcard
   for (let i = 0; i < 4; i++) {
@@ -39,9 +39,9 @@ export function getValues(): (0.00200 | 0.00200 | 0.25| 0.00004 | 0.00004)[] {
 // Value  Probability
     // 0.00200     51%   0.51
     // 0.00200      30%   0.3
-    // 0.25     14%   0.14
+    // 0.00004     14%   0.14
     // 0.00004     4%   0.04
-    // 0.00004    1%   0.01
+    // 0.25    1%   0.01
 
     // Assign a reward value based on the generated random number
     if (randomNumber < 0.5) {
@@ -49,11 +49,11 @@ export function getValues(): (0.00200 | 0.00200 | 0.25| 0.00004 | 0.00004)[] {
     } else if (randomNumber < 0.8) {
       values.push(0.00200);
     } else if (randomNumber < 0.95) {
-      values.push(0.25);
+      values.push(0.00004);
     } else if (randomNumber < 0.99) {
       values.push(0.00004);
     } else {
-      values.push(0.00004);
+      values.push(0.25);
     }
   }
 
