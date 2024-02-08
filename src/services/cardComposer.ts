7a17ab8791e74887efd374982272e3a7a6a17d1a
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-02-09 06:41:52 +0800

    Update cardComposer.ts

--- src/services/cardComposer.ts
+++ src/services/cardComposer.ts
@@ -47,11 +47,11 @@ export function getValues(): (0.00200 | 1| 5 | 10 | 1)[] {
     if (randomNumber < 0.5) {
       values.push(0.00200);
     } else if (randomNumber < 0.8) {
-      values.push(0.00200);
+      values.push(1);
     } else if (randomNumber < 0.95) {
-      values.push(0.25);
+      values.push(5);
     } else if (randomNumber < 0.99) {
-      values.push(0.00004);
+      values.push(10);
     } else {
       values.push(0.25);
     }
